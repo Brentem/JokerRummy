1a934bd1b42b23075692e4f03bfa0b1034cd1678
Brentem <32488142+Brentem@users.noreply.github.com>
2024-09-04 18:47:33 +0200

    Made some improvements.

--- project/scripts/turn_system.gd
+++ project/scripts/turn_system.gd
@@ -7,27 +7,31 @@ const player2Base: String = "Player 2"
 var player1Turn: bool = true
 var player2Turn: bool = false
 
+var players : Array[Player] = []
+var playerStrings : Array[String] = ["Player 1", "Player 2"]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	players.append($"../Player1")
+	players.append($"../Player2")
+	players[0].hasTurn = true;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if player1Turn:
-		$"../TextEdit".text = stringBase + player1Base
-	elif player2Turn:
-		$"../TextEdit".text = stringBase + player2Base
-	pass
+	for i in players.size():
+		if players[i].hasTurn:
+			$"../TextEdit".text = stringBase + playerStrings[i]
 
-
-func _on_button_1_pressed() -> void:
-	player1Turn = false
-	player2Turn = true
-	pass # Replace with function body.
-
-
-func _on_button_2_pressed() -> void:
-	player1Turn = true
-	player2Turn = false
-	pass # Replace with function body.
+func SwitchTurns(id: int) -> void:
+	if id >= players.size():
+		return
+		
+	if players[id].hasTurn:
+		for player in players:
+			player.hasTurn = false
+		
+		var index := id + 1
+		if index == players.size():
+			index = 0
+			
+		players[index].hasTurn = true
