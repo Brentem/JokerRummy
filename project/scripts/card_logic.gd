9b825bfdc4faaae30429ab1a2918c26011778e39
Brent van de Pol <32488142+Brentem@users.noreply.github.com>
2024-12-24 17:54:46 +0100

    Created private _isQuartet function.

--- project/scripts/card_logic.gd
+++ project/scripts/card_logic.gd
@@ -8,7 +8,11 @@ const QuartetSize : int = 4
 static func foo(cards: Array[CardInfo]) -> bool:
 	# TODO: 40 points already laid out or not?
 	
-	# TODO: Is it a quartet or 3 cards of it?
+	return _isQuartet(cards) || _isSeries(cards)
+
+static func _isQuartet(cards: Array[CardInfo]) -> bool:
+	var retVal : bool = false
+	
 	if cards.size() <= QuartetSize:
 		var firstCard := cards[0]
 		var symbolInfo : Array[bool]
@@ -18,7 +22,20 @@ static func foo(cards: Array[CardInfo]) -> bool:
 			var temp : bool = cards[i]._symbol == firstCard._symbol
 			symbolInfo.append(temp)
 			suitCounter[cards[i]._suit - 1] += 1
-	
-	# TODO: Is it a series or part of it?
-	
+		
+		var counter : int = 0
+		for i in QuartetSize:
+			if suitCounter[i] > 1:
+				retVal = false
+				break
+			elif symbolInfo[i] && suitCounter[i] == 1:
+				counter += 1
+		
+		retVal = counter >= 3
+	else:
+		return false
+		
+	return retVal
+
+static func _isSeries(cards: Array[CardInfo]) -> bool:
 	return true
